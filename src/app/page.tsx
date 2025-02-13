803f7c49b4edb894bb310506d0f6b3fb2f6ddf26
Azrael555 <hellkittenazrael@gmail.com>
2025-02-13 11:34:21 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Slideshow from "./slideshow";
 import SearchParamsComponent from "./SearchParamsComponent";
 import { useRouter } from "next/navigation";
 
-const socket = io("https://your-backend.onrender.com", {
+const socket = io("https://findom-chess.onrender.com", {
   transports: ["websocket"], 
 });
 
